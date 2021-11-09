70d49aed78d04dd9ee4a8a632510d553f7c34f18
jarltuxen <jarl.tuxen@gmail.com>
2021-11-09 15:52:56 +0100

    paging and sort

--- src/main/java/dk/kea/regionkommune/controller/RestApiController.java
+++ src/main/java/dk/kea/regionkommune/controller/RestApiController.java
@@ -5,9 +5,17 @@ import dk.kea.regionkommune.model.Kommune;
 import dk.kea.regionkommune.model.Region;
 import dk.kea.regionkommune.repository.KommuneRepository;
 import dk.kea.regionkommune.repository.RegionRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin
@@ -42,5 +50,54 @@ public class RestApiController {
         return kommuneRepository.findKommuneByRegionRegionKode(regionKode);
     }
 
+    @GetMapping("/kommunepageparm")
+    public ResponseEntity<Map<String, Object>> getPageOfKommuner(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
 
+        //PageNo and Size
+        Pageable paging = PageRequest.of(page, size);
+        Page<Kommune> pageKommune = kommuneRepository.findAll(paging);
+
+        List<Kommune> kommuner = pageKommune.getContent();
+
+        if (kommuner.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("kommuner", kommuner);
+        response.put("currentPage", pageKommune.getNumber());
+        response.put("totalItems", pageKommune.getTotalElements());
+        response.put("totalPages", pageKommune.getTotalPages());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/kommunesortp")
+    public ResponseEntity<Map<String, Object>>  getScreeningsSortAndPage (
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "3") int size,
+            @RequestParam(defaultValue = "kommuneKode") String sortBy,
+            @RequestParam(defalutValue = "asc") String sortOrder
+    ){
+
+        //sortOrder and sortBy
+        Sort _sortBy = Sort.by(sortBy);
+        Sort _sortOrder = Sort.by(sortOrder);
+        Sort orders = _sortBy.and(_sortOrder);
+        Pageable pagingSort = PageRequest.of(page, size, Sort.by(orders));
+        Page<Kommune> pageScreens = kommuneRepository.findAll(pagingSort);
+        List<Kommune> kommuner = pageScreens.getContent();
+
+        if (kommuner.isEmpty()) return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("kommuner", kommuner);
+        response.put("currentPage", pageScreens.getNumber());
+        response.put("totalItems", pageScreens.getTotalElements());
+        response.put("totalPages", pageScreens.getTotalPages());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
